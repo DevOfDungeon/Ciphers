c5a969d922fb7d22a2758f6b0a253c11a1c0d340
DevOfDungeon <262024899+DevOfDungeon@users.noreply.github.com>
2026-06-12 12:18:58 +0000

    Introduced alphabet to the code.Added implementation of a variable which contains shifted alphabet. Built translation table linking alphabet with the shifted ones, keeping both upper and lower characters.

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -6,3 +6,14 @@ def caesar(text, shift, encrypt=True):
     if shift < 1 or shift > 25:
         raise ValueError("Shift must be between 1 and 25.")
     
+    alphabet ='abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = -shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    
+    return encrypted_text
+
