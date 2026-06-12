e3fdbca9e514f6838b31eb4c2adde0774e89971b
DevOfDungeon <262024899+DevOfDungeon@users.noreply.github.com>
2026-06-12 12:26:06 +0000

    Removed test for checking if shift is integer as user input will be collected using int(input())

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -1,8 +1,6 @@
 def caesar(text, shift, encrypt=True):
-    if not isinstance(shift, int):
-        raise ValueError("Shift must be an integer.")
     if shift < 1 or shift > 25:
-        raise ValueError("Shift must be between 1 and 25.")
+        raise ValueError("Shift must be an integer between 1 and 25.")
     
     alphabet ='abcdefghijklmnopqrstuvwxyz'
 
