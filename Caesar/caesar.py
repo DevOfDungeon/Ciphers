e35729cce77c15419388d87752d1e2674989bb20
DevOfDungeon <262024899+DevOfDungeon@users.noreply.github.com>
2026-06-12 12:21:06 +0000

    Removed test for checking if text is string as it will be user input which is by default of string data type.

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -1,6 +1,4 @@
 def caesar(text, shift, encrypt=True):
-    if not isinstance(text, str):
-        raise ValueError("Text must be a string.")
     if not isinstance(shift, int):
         raise ValueError("Shift must be an integer.")
     if shift < 1 or shift > 25:
