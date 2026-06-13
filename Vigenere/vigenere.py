ef5cff5f234508d399fb0c616eec11b7338c4bea
DevOfDungeon <262024899+DevOfDungeon@users.noreply.github.com>
2026-06-13 17:28:23 +0000

    Added vigenere cipher general  implementation

---
+++ Vigenere/vigenere.py
@@ -0,0 +1,25 @@
+def vigenere(text,key,direction=1):
+    
+    if not key.isalpha():
+        raise ValueError("Key must consist of alphabetic characters only.")
+    
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final =''
+    key_index = 0
+
+    for char in text:
+   
+    # Append any non letter to the final string without changing it
+        if not char.isalpha():
+            final += char
+        else:
+            # Find the right key character to use
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset= alphabet.index(key_char)
+            index=alphabet.find(char)
+            new_index= (index + direction * offset) % len(alphabet)
+            final += alphabet[new_index]
+    return final
